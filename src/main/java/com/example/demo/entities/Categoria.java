e521ef752687ae25dc18d8dff965abdeea732fd9
johnfredyb <johnfredybb@ufps.edu.co>
2023-04-21 12:12:37 -0500

    Creación de repository para conexion a la base de datos

--- src/main/java/com/example/demo/entities/Categoria.java
+++ src/main/java/com/example/demo/entities/Categoria.java
@@ -1,10 +1,19 @@
 package com.example.demo.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import lombok.Data;
 
 @Data
+@Entity
 public class Categoria {
 	
+	@Id
+	@SequenceGenerator(name="categoria_id_seq", allocationSize=1)
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="categoria_id_seq")
 	private Integer id;
 	
 	private String descripcion;
